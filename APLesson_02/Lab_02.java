54a2e2f125871af2ed9ef71c34753a710d4dd67f
joshir7768 <JoshiR7768@sduhsd.lan>
2016-09-06 12:12:23 -0700

    Completed lab 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,37 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 2;
+		int b = 4;
+		int recArea = a * b;
+		
+		System.out.println("2 multiplied by 4 is" + " " + recArea);
+		
+		int c = 5;
+		int d = 4;
+		int product = c * d; 
+		
+		System.out.println("5 and 4 would produce..." + " " + product); 
+		
+		
+		int e = 5;
+		int f = 6;
+		int sqArea = e * f;
+		
+		System.out.println("5 multiplied by 6 is" + " " + sqArea);
+		
+		string g = Rachana Joshi;
+		string h = Tierra Vista Circle;
+		string i = San Diego;
+		string j = 92130; 
+		
+		
+		System.out.println("Name - " + g);
+		System.out.println("Address - " + h);
+		System.out.println("City - " + i);
+		System.out.println("zip - " + j); 
+	}
+    
+
+}
