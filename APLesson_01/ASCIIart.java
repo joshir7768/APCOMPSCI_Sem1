ceff79401ba8a769b0de5067a618ae654b73bdfb
joshir7768 <JoshiR7768@sduhsd.lan>
2016-09-06 11:07:11 -0700

    AddedFiles

---
+++ APLesson_01/ASCIIart.java
@@ -0,0 +1,27 @@
+public class ASCIIart 
+{
+	public static void main (String[]args)
+	{
+		System.out.println("  __________________ ");
+		System.out.println(" |  ______________  | ");
+		System.out.println(" | |              | | ");
+		System.out.println(" | |    0     0   | | ");
+		System.out.println(" | |       -      | | ");
+        System.out.println(" | |    \\___/     | | ");
+        System.out.println(" |  --------------| |");
+        System.out.println("  -------------------");
+        System.out.println(" /                   \\");
+        System.out.println("/                     \\");
+        System.out.println("/                      \\");
+        System.out.println("------------------------");
+
+        System.out.println("    () _ () ");
+		System.out.println("    (^ . ^) ");
+		System.out.println("   ,(\") (\"), ");
+		
+	
+		
+	}
+	
+	
+}
