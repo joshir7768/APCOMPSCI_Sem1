507a8533faaf8477da96674379a8eac8de45426d
joshir7768 <joshir7768@gmail.com>
2016-11-18 12:12:20 -0800

    add

---
+++ APLesson_09/Lab 09/FibonaciSequence.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class FibonaciSequence
+{
+	static String[] num;
+	static String[] size;
+	
+	public static void main(String[]args)
+	{
+		String [] seq = new String[2];
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter your starying number: ");
+		System.out.println("Please enter your sequence size: ");
+		
+		for(int i = 0; i < seq.length; i++)
+		{
+			seq[i] = kb.next();
+			if i = 0 || 1
+			{
+				seq 
+			}
+		}
+		
+	}
+}
--- APLesson_09/Lab 09/Reverseword.java
+++ APLesson_09/Lab 09/Reverseword.java
@@ -1,42 +1,32 @@
 import java.util.Scanner;
 public class Reverseword
-{
-	static String word1;
-	static String word2;
-	static String word3;
-	static String word4;
-	static String word5;
-	static String stuff;
-	
+{	
+	static String[] words;
 	public static void main(String[]args)
 	{
-		String[] words = new String[5];
+		String [] words = new String[5];
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Enter first word: ");
-		word1 = kb.nextInt();
-		System.out.println("Enter second word: ");
-		word2 = kb.nextInt();
-		System.out.println("Enter third word: ");
-		word3 = kb.nextInt();
-		System.out.println("Enter fourth word: ");
-		word4 = kb.nextInt();
-		System.out.println("Enter fifth word: ");
-		word5 = kb.nextInt();
-		Reverse();
-		System.out.println("In order... " + stuff );
+		System.out.println("Enter 5 words: ");
 		
 		for(int i = 0; i < words.length; i++)
 		{
-			words [i] = kb.next();
+			words[i] = kb.next();
 		}
 		
+		System.out.println("In order... ");
 		for (String word : words)
 		{
 			System.out.println("\"" + word + "\" " );
 		}
+		System.out.println("In reverse...");
+		Reverse(words);
 	}
-	public static void Reverse()
+	public static void Reverse(String [] words)
 	{
+		for(int i = words.length-1; i >= 0; i--)
+		{
+			System.out.println(words[i]);
+		}
 		
 	}
 }
