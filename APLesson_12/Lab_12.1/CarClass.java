7874bf4b283420bfb5b487b4cdd44d32be6dae7f
joshir7768 <joshir7768@gmail.com>
2017-01-26 10:49:12 -0800

    Update CarClass.java

--- APLesson_12/Lab_12.1/CarClass.java
+++ APLesson_12/Lab_12.1/CarClass.java
@@ -1,52 +1,59 @@
 public class CarClass
 {
-	private String p, i, e, t;
-	
-	public CarClass()
-	{
-		p = ""; 
-		i = "";
-		e = "";
-		t = "";
-	}
-	
-		public CarClass( String paint, String interior, String engine, String tires)
-	{	
-		p = paint; 
-		i = interior;
-		e = engine;
-		t = tires;
-	}
-	
-	public void setCustom( String paint, String interior, String engine, String tires)
-	{	
-		p = paint; 
-		i = interior;
-		e = engine;
-		t = tires;
-	}
-	
-	public String getPaint()
-	{
-		return p;
-	}
-	
-	public String getInterior()
-	{
-		return i;
-	}
-	
-	public String getEngine()
-	{
-		return e;
-	}
-	
-	public String getTires()
-	{
-		return t;
-	}
-	
-	
-	
-	
+    private String p, i, e, t;
+    
+    public CarClass()
+    {
+        p = ""; 
+        i = "";
+        e = "";
+        t = "";
+    }
+    
+    public CarClass( String paint, String interior, String engine, String tires)
+    {   
+        p = paint; 
+        i = interior;
+        e = engine;
+        t = tires;
+    }
+    
+    public void setCustom( String paint, String interior, String engine, String tires)
+    {   
+        p = paint; 
+        i = interior;
+        e = engine;
+        t = tires;
+    }
+    
+    public String getPaint()
+    {
+        return p;
+    }
+    
+    public String getInterior()
+    {
+        return i;
+    }
+    
+    public String getEngine()
+    {
+        return e;
+    }
+    
+    public String getTires()
+    {
+        return t;
+    }
+    
+    public String toString()
+    {
+        return "Your vehicle is ready... \nPaint: " + p+ 
+                                         "\nInterior: " + i+
+                                         "\nEngine: " + e+
+                                         "\nTires: " + t;
+    }
+    
+    
+    
 }
