2b122a6965dbc392d45abef9f03b512ff9a0e2ca
joshir7768 <joshir7768@gmail.com>
2017-01-26 10:49:34 -0800

    Update CarDriver.java

--- APLesson_12/Lab_12.1/CarDriver.java
+++ APLesson_12/Lab_12.1/CarDriver.java
@@ -1,30 +1,22 @@
 import java.util.Scanner;
 public class CarDriver
 {
-	public static void main(String[]args)
-		{
-			UserNames object = new UserNames("paint", "interior", "engine", "tires");
-		
-			System.out.println("Type in the car's color");
-			String paint = kb.next();
-			//keep on asking questions about the interior, engine and tires
-			//keep the usernames object1 = new..
-			//keep name: + object...
-			
-			UserNames object1 = new UserNames();
-			System.out.println("Name: " + object1.getPaint());
-		
-		
-		
-		
-		
-		
-			System.out.println("<<<<<<<<<<<<<<<<<< USER INFO >>>>>>>>>>>>>>>>>>>>");
-			System.out.println("Name: " + object1.getfirstName() + " " + object1.getlastName());
-		
-			object.setUserName("pHandsome");
-			System.out.println("<<<<<<<<<<<<<<<<<< USER INFO >>>>>>>>>>>>>>>>>>>>");
-			System.out.println("Name: " + object.getfirstName() + " " + object.getlastName());
-			System.out.println("User Name: " + object.getUserName());
-		}	
-}
+    public static void main(String[]args)
+    {        
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Type in the car's color");
+        String paint = kb.nextLine();
+        System.out.println("Type in the car's interior");
+        String interior = kb.nextLine();
+        System.out.println("Type in the car's engine");
+        String engine = kb.nextLine();
+        System.out.println("Type in the car's tires");
+        String tires = kb.nextLine();
+        
+        CarClass object = new CarClass(paint, interior, engine, tires);
+        System.out.println(object);
+        
+        object.setCustom("red", "tan leather", "V8", "sport");
+        System.out.println(object);
+    }        
+}   
