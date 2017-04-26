52b09cd118807cc57d74abfedb684781ff627960
joshir7768 <joshir7768@gmail.com>
2017-04-26 12:15:25 -0700

    add

--- PictureLab_Student/classes/PictureExplorer.java
+++ PictureLab_Student/classes/PictureExplorer.java
@@ -799,7 +799,7 @@ public class PictureExplorer implements MouseMotionListener, ActionListener, Mou
    */
   public static void main( String args[])
   {
-    Picture pix = new Picture("beach.jpg");
+    Picture pix = new Picture("puppy.jpg");
     pix.explore();
   }
   
