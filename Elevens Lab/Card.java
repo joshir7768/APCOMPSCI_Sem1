43d0d9291d7594a72bf8b7104fe73217d99d6317
joshir7768 <joshir7768@gmail.com>
2017-05-10 12:10:44 -0700

    add

--- Elevens Lab/Card.java
+++ Elevens Lab/Card.java
@@ -3,7 +3,8 @@
  *
  * <code>Card</code> represents a playing card.
  */
-public class Card {
+public class Card 
+{
 
 	/**
 	 * String value that holds the suit of the card
@@ -31,57 +32,44 @@ public class Card {
 	 * @param cardPointValue an <code>int</code> value
 	 *                  containing the point value of the card
 	 */
-	public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	public Card(String cardRank, String cardSuit, int cardPointValue)
+	{
+		this.suit = cardSuit;
+        this.rank = cardRank;
+        this.pointValue = cardPointValue;
+    }
+	
+	public String suit() 
+	{
+	
+		return this.suit;
+		
+    }
+
+	public String rank() 
+	{
+		
+        return this.rank;
 	}
 
-
-	/**
-	 * Accesses this <code>Card's</code> suit.
-	 * @return this <code>Card's</code> suit.
-	 */
-	public String suit() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-   }
-
-	/**
-	 * Accesses this <code>Card's</code> rank.
-	 * @return this <code>Card's</code> rank.
-	 */
-	public String rank() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	public int pointValue() 
+	{
+		return this.pointValue;
 	}
 
-   /**
-	 * Accesses this <code>Card's</code> point value.
-	 * @return this <code>Card's</code> point value.
-	 */
-	public int pointValue() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	public boolean matches(Card otherCard) 
+	{
+		if (this.suit.equals(otherCard.suit) && this.rank.equals(otherCard.rank) && this.pointValue == otherCard.pointValue) 
+		{
+           return true;
+        }
+        return false;
 	}
 
-	/** Compare this card with the argument.
-	 * @param otherCard the other card to compare to this
-	 * @return true if the rank, suit, and point value of this card
-	 *              are equal to those of the argument;
-	 *         false otherwise.
-	 */
-	public boolean matches(Card otherCard) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
-
-	/**
-	 * Converts the rank, suit, and point value into a string in the format
-	 *     "[Rank] of [Suit] (point value = [PointValue])".
-	 * This provides a useful way of printing the contents
-	 * of a <code>Deck</code> in an easily readable format or performing
-	 * other similar functions.
-	 *
-	 * @return a <code>String</code> containing the rank, suit,
-	 *         and point value of the card.
-	 */
 	@Override
-	public String toString() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	public String toString() 
+	{
+		return this.rank + " of " + this.suit + " (point value = " + this.pointValue + ")";
 	}
+
 }
